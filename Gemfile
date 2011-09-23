6a4e276597025e35683ca079fe708218a6cc2429
fidel jose villero de hoyos <fidel@dinamic-tigo186-97-128-50.tigo.com.co>
2011-09-22 21:04:31 -0500

    un nuevo mensaje undefined method -friendly_id

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.0'
+gem 'rails', '3.0.10'
 gem 'jquery-rails'
-gem "friendly_id"
+gem "friendly_id", "~> 4.0.0.beta8"
 gem 'sqlite3', '1.3.4'
 gem 'haml',  '3.1.2'
 gem 'devise', '1.4.5'
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,11 @@ class Post < ActiveRecord::Base
   validates_presence_of :title, :message => 'pleace is require'
   validates_presence_of :content, :message => 'pleace is required'
   
-  extend FriendlyId
-  friendly_id :title, :use => :slugged
-  
-  #has_many :urls, :autosave => true
-  #validar q un record hijo se asocia a un record padre
   has_many :urls #, :inverse_of => :posts
   accepts_nested_attributes_for :urls
-  #attr_accessible :name, :posts_attributes
+  
+  extend FriendlyId
+  friendly_id :title, :use => :slugged
   
   
 end
